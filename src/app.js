2dadeaa46eaa1bc23f12cedcd791785f45a6a7b2
KauaVilasBoas <kauacaldeira@hotmail.com>
2023-09-12 21:58:44 -0300

    usando a biblioteca dotenv

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import conectaInDataBase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
 
 const conexao = await conectaInDataBase();
 
@@ -14,29 +15,13 @@ conexao.once("open", () => {
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos aneis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
-
 app.get("/", (req, res) => {
     res.status(200).send("Curso de node.js");
 });
 
-app.get("/livros", (req, res) =>{
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) =>{
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
